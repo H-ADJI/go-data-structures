75f78cb56f794740f00dd0d8b0370ba4b2a4e710
khalil <h-adji_tech@proton.me>
2024-03-02 12:06:21 +0100

    added some test for linked lists

---
+++ test/linked_list_test.go
@@ -0,0 +1,32 @@
+package linked_list_test
+
+import (
+	"testing"
+
+	linked_list "github.com/H-ADJI/go-data-structures/linked_list"
+)
+
+func TestNew(t *testing.T) {
+	list := linked_list.New()
+	if list == nil {
+		t.Fatalf(`Wanted non-nil pointer for linked list, got %p pointer`, list)
+	}
+}
+func TestSearch(t *testing.T) {
+	list := linked_list.New()
+	if list.Search(0) {
+		t.Fatalf(`List should be emnpty`)
+	}
+	if list == nil {
+		t.Fatalf(`Wanted non-nil pointer for linked list, got %p pointer`, list)
+	}
+}
+func TestFromArray(t *testing.T) {
+	array := []int{2, 3, 5, 3, 2, 1, 6, 9, 4, 232, 43, 2, 0}
+	list := linked_list.New().FromArray(array)
+	for _, element := range array {
+		if !list.Search(element) {
+			t.Fatalf("Element %v should be in the list", element)
+		}
+	}
+}
