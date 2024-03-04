c85c55b13226f79d409aa164fb3d476661220b9c
khalil <h-adji_tech@proton.me>
2024-03-04 23:07:12 +0100

    sort test broken

--- main.go
+++ main.go
@@ -12,14 +12,6 @@ func main() {
 	array := []int{2, 3, 5, 3, 2, 1, 6, 9, 4, 232, 43, 2, 0}
 	list := linkedList.New()
 	list.FromArray(array)
-	fmt.Println(list)
-	iterator := list.Iterator()
-	if iterator.HasNext() {
-		preceding := iterator.GetNext()
-		current := iterator.GetNext()
-		following := iterator.GetNext()
-		current.Swap(preceding, following)
-
-	}
+	list.Sort()
 	fmt.Println(list)
 }
--- test/linked_list_test.go
+++ test/linked_list_test.go
@@ -43,6 +43,30 @@ func TestSearch(t *testing.T) {
 		t.Fatalf(`List should be emnpty`)
 	}
 }
+func TestSort(t *testing.T) {
+	array := []int{1, 2, 3, 4}
+	list := linkedList.New()
+	list.FromArray(array)
+	list.Sort()
+	arrayExpected := []int{1, 2, 3, 4}
+	for i := range arrayExpected {
+		if array[i] != arrayExpected[i] {
+			t.Fatalf("\nexpected ->> %v \nbut got ->> %v", arrayExpected, array)
+		}
+
+	}
+	array = []int{1, 3, 2, 4}
+	list = linkedList.New()
+	list.FromArray(array)
+	list.Sort()
+	arrayExpected = []int{1, 2, 3, 4}
+	for i := range arrayExpected {
+		if array[i] != arrayExpected[i] {
+			t.Fatalf("\nexpected ->> %v \nbut got ->> %v", arrayExpected, array)
+		}
+
+	}
+}
 func TestFromArray(t *testing.T) {
 	array := []int{2, 3, 5, 3, 2, 1, 6, 9, 4, 232, 43, 2, 0}
 	list := linkedList.New()
