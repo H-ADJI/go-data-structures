ef90c49afef72e0e54ff4fb062cdde0ebcfd08ef
h-adji <h-adji_tech@proton.me>
2024-04-28 11:36:14 +0200

    first impl for circular queue

---
+++ queue/circular_queue.go
@@ -0,0 +1,55 @@
+package queue
+
+import "errors"
+
+const QueueCapacity = 10
+
+type CircularQueue struct {
+	buffer [QueueCapacity]int
+	front  int
+	rear   int
+}
+
+func (q CircularQueue) Dequeue() (int, error) {
+	if q.IsEmpty() {
+		return 0, errors.New("the queue is empty")
+	}
+
+	e := q.buffer[q.front]
+	q.front = (q.front + 1) % (QueueCapacity - 1)
+	return e, nil
+}
+
+func (q CircularQueue) Enqueue(e int) error {
+	if q.IsFull() {
+		return errors.New("the queue is full")
+	}
+	q.rear = (q.rear + 1) % (QueueCapacity - 1)
+	q.buffer[q.rear] = e
+	return nil
+}
+
+func (q CircularQueue) occupiedSpace() int {
+	if q.IsEmpty() {
+		return 0
+	}
+	if q.rear >= q.front {
+		return q.rear - q.front + 1
+	} else {
+		return QueueCapacity + q.rear - q.front + 1
+	}
+
+}
+func (q CircularQueue) IsFull() bool {
+	return q.occupiedSpace() == QueueCapacity
+}
+
+func (q CircularQueue) IsEmpty() bool {
+	if q.front == -1 && q.rear == -1 {
+		return true
+	}
+	return false
+}
+func (q CircularQueue) Peek() (int, error) {
+	return 0, nil
+}
