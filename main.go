d0adc69c72230a8525d62a5fe67e738ae899cbc3
h-adji <h-adji_tech@proton.me>
2024-05-10 12:12:03 +0200

    binary tree impl + traversal + visualization

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/H-ADJI/go-data-structures/queue"
-)
-
 func main() {
-	q := queue.NewQueue(5)
-	q.Enqueue(1)
-	q.Enqueue(2)
-	fmt.Println(q)
-	q.Dequeue()
-	q.Enqueue(3)
-	fmt.Println(q)
-	q.Dequeue()
-	q.Enqueue(4)
-	q.Enqueue(5)
 
-	fmt.Println(q)
 }
